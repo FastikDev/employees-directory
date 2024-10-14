47982c11e52cd6f7c9efde1ac363f82111108a86
FastikDev <sir.fastik18@gmail.com>
2024-10-15 02:34:21 +0300

    fix build directory

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = (env, argv) => {
     entry: './src/index.tsx',
     output: {
       filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      path: path.resolve(__dirname, 'build'),
       clean: true,
     },
     module: {
