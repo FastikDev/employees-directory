0a92be71e4b0eaa40fb3850ed6ce1b0413fdb5e3
FastikDev <sir.fastik18@gmail.com>
2024-10-16 02:32:56 +0300

    add offline status

--- src/features/Header/index.tsx
+++ src/features/Header/index.tsx
@@ -1,12 +1,42 @@
 import React, { useState, useEffect } from 'react';
+import { TypedUseQuery } from '@reduxjs/toolkit/query/react';
 import { useSearchParams } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import SearchBar from './components/SearchBar';
+import Refresh from './components/Refresh';
+import { useMediaQuery } from 'react-responsive';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
 import './index.scss';
 
 const Header = () => {
-  const [searchValue, setSearchValue] = useSearchParams();
+  const isMobile = useMediaQuery({ maxWidth: 1279 });
+
+  const [searchValue] = useSearchParams();
+  const [loading, setLoading] = useState(true);
   const [searchText, setSearchText] = useState<string>(searchValue.get('search') || '');
+  const [isNetwork, setIsNetwork] = useState(true);
+
+  const { loading: employessLoading } = useSelector((state: RootState) => state.employees);
+
+  useEffect(() => {
+    const updateNetworkStatus = () => {
+      setIsNetwork(navigator.onLine);
+    };
+    window.addEventListener('online', updateNetworkStatus);
+    window.addEventListener('offline', updateNetworkStatus);
+
+    updateNetworkStatus();
+
+    return () => {
+      window.removeEventListener('online', updateNetworkStatus);
+      window.removeEventListener('offline', updateNetworkStatus);
+    };
+  }, []);
+
+  useEffect(() => {
+    setLoading(employessLoading === 'loading');
+  }, [employessLoading]);
 
   useEffect(() => {
     const currentSearch = searchValue.get('search') || '';
@@ -26,10 +56,25 @@ const Header = () => {
   return (
     <>
       <header className="header">
-        <h1 className="header__title">Search</h1>
-        <SearchBar onSearchChange={handleSearching} />
+        {loading && (
+          <div className="loading">
+            <h1 className="header__title header__title_offline">Search</h1>
+            <p className="error-network__description">Wait a second, I'm loading...</p>
+          </div>
+        )}
+        {!loading && !isNetwork && (
+          <div className="error-network">
+            <h1 className="header__title header__title_offline">Search</h1>
+            <p className="error-network__description">
+              I can't update my data. Check your internet connection.
+            </p>
+          </div>
+        )}
+        {!loading && isNetwork && <h1 className="header__title">Search</h1>}
+        {isNetwork && <SearchBar onSearchChange={handleSearching} />}
         <Navigation />
       </header>
+      {loading && isMobile && <Refresh />}
     </>
   );
 };
